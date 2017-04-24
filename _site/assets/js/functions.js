a5cfe0df7f47660cf07994d355f28cfecef18945
Michael Doyle <mikedoyle007@gmail.com>
2017-04-23 21:10:26 -0700

    Smooth Scrolling
    Added code from one of devtips pull requests for smooth scrolling.

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,5 +1,21 @@
 $( document ).ready(function() {
 
   // Get started!
+  smoothScroll(300)
 
 });
+
+// smoothScroll function is applied from the document ready function
+function smoothScroll (duration) {
+  $('a[href^="#"]').on('click', function(event) {
+
+      var target = $( $(this).attr('href') );
+
+      if( target.length ) {
+          event.preventDefault();
+          $('html, body').animate({
+              scrollTop: target.offset().top
+          }, duration);
+      }
+  });
+}
